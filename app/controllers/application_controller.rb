7f9533f5e2d166b11d1615b10657dedca4feb78c
Nelson Davis <daslen@yahoo.com>
2013-07-29 20:53:19 -0700

    add mobile detection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
+    helper :all
+    protect_from_forgery
 
-	protect_from_forgery
+    private
+    def mobile_device?
+      request.user_agent =~ /Mobile|webOS/
+    end
+    helper_method :mobile_device?
 
 end
--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -1,5 +1,10 @@
 class CatalogController < ApplicationController
   def home
+    respond_to do |format|
+      format.html
+      format.mobile
+    end
+
   end
 
   def mobile
